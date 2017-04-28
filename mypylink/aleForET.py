140c4f340d3eda9410748cc66374d4c4b2e7d5d1
Zhuode Liu <zhuode93@cs.utexas.edu>
2017-04-27 21:35:10 -0500

    add "step by step" mode to play ATARI"

--- mypylink/aleForET.py
+++ mypylink/aleForET.py
@@ -17,12 +17,12 @@ class aleForET:
         self.ale = ALEInterface()
         GAME_W, GAME_H = 160, 210
 
-        # Setting up the pygame screen Surface 
+        # Setting up the pygame screen Surface
         self.size = GAME_W * V.xSCALE, GAME_H * V.ySCALE
 
         # Get & Set the desired settings
         self.ale.setInt('random_seed', 123)
-        self.ale.setBool('sound', False) 
+        self.ale.setBool('sound', False)
         self.ale.setBool('display_screen', False)
         self.ale.setBool('color_averaging', False)
         self.ale.setFloat('repeat_action_probability', 0.0)
@@ -34,57 +34,121 @@ class aleForET:
         # Get the list of legal actions
         self.legal_actions = self.ale.getLegalActionSet()
 
-    def run(self, gc_window_drawer_func = None, save_screen_func = None, event_handler_func = None):
+    def run(self, gc_window_drawer_func = None, save_screen_func = None, event_handler_func = None, record_a_and_r_func = None):
         last_time=time.time()
         frame_cnt=0
         clock = pygame.time.Clock()
         # Play 10 episodes
         for episode in xrange(10):
-          total_reward = 0
-          while not self.ale.game_over():
-            clock.tick(30) # control FPS
-            frame_cnt+=1
+            total_reward = 0
+            while not self.ale.game_over():
+                clock.tick(30) # control FPS
+                frame_cnt+=1
 
-            key = pygame.key.get_pressed()
-            a_index = aenum.action_map(key, self.gamename)
-            a = self.legal_actions[a_index]
+                key = pygame.key.get_pressed()
+                if event_handler_func != None:
+                    stop, eyelink_err_code, bool_drawgc = event_handler_func(key)
+                    if stop:
+                        return eyelink_err_code
 
-            if event_handler_func != None:
-                stop, eyelink_err_code, bool_drawgc = event_handler_func(key, a)
-                if stop:
-                    return eyelink_err_code
+                # Display FPS
+                diff_time = time.time()-last_time
+                if diff_time > 1.0:
+                    print 'FPS: %.1f' % clock.get_fps()
+                    last_time=time.time()
 
-            # Display FPS
-            diff_time = time.time()-last_time
-            if diff_time > 1.0:
-                print 'FPS: %.1f' % clock.get_fps()
-                last_time=time.time()
-                
-            # Show game image
-            cur_frame_np = self.ale.getScreenRGB()
-            cur_frame_Surface = pygame.surfarray.make_surface(cur_frame_np)
-            cur_frame_Surface = pygame.transform.flip(cur_frame_Surface, True, False)
-            cur_frame_Surface = pygame.transform.rotate(cur_frame_Surface, 90)
-            # Perform scaling directly on screen, leaving cur_frame_Surface unscaled.
-            # Slightly faster than scaling cur_frame_Surface and then transfer to screen.
-            pygame.transform.scale(cur_frame_Surface, self.size, self.screen)
+                # Show game image
+                cur_frame_np = self.ale.getScreenRGB()
+                cur_frame_Surface = pygame.surfarray.make_surface(cur_frame_np)
+                cur_frame_Surface = pygame.transform.flip(cur_frame_Surface, True, False)
+                cur_frame_Surface = pygame.transform.rotate(cur_frame_Surface, 90)
+                # Perform scaling directly on screen, leaving cur_frame_Surface unscaled.
+                # Slightly faster than scaling cur_frame_Surface and then transfer to screen.
+                pygame.transform.scale(cur_frame_Surface, self.size, self.screen)
 
-            if gc_window_drawer_func != None and bool_drawgc:
-                gc_window_drawer_func(self.screen)
-            pygame.display.flip()
+                if gc_window_drawer_func != None and bool_drawgc:
+                    gc_window_drawer_func(self.screen)
+                pygame.display.flip()
 
-            # Save frame to disk (160*210, i.e. not scaled; because this is faster)
-            if save_screen_func != None:
-                save_screen_func(cur_frame_Surface, frame_cnt)
+                # Save frame to disk (160*210, i.e. not scaled; because this is faster)
+                if save_screen_func != None:
+                    save_screen_func(cur_frame_Surface, frame_cnt)
 
-            # Apply an action and get the resulting reward
-            reward = self.ale.act(a);
-            total_reward += reward
-            
-            pygame.event.pump() # need this line to get new key pressed
+                # Apply an action and get the resulting reward
+                a_index = aenum.action_map(key, self.gamename)
+                a = self.legal_actions[a_index]
+                reward = self.ale.act(a);
+                total_reward += reward
+                if record_a_and_r_func != None:
+                    record_a_and_r_func(a, reward)
 
-          print 'Episode', episode, 'ended with score:', total_reward
-          self.ale.reset_game()
+                pygame.event.pump() # need this line to get new key pressed
+
+            print 'Episode', episode, 'ended with score:', total_reward
+            self.ale.reset_game()
+
+        TRIAL_OK = 0 # copied from EyeLink's constant
+        return TRIAL_OK
+
+    def run_in_step_by_step_mode(self, gc_window_drawer_func = None, save_screen_func = None, event_handler_func = None, record_a_and_r_func = None):
+        frame_cnt=0
+        bool_drawgc = False
+        clock = pygame.time.Clock()
+        # Play 10 episodes
+        for episode in xrange(10):
+            total_reward = 0
+            while not self.ale.game_over():
+
+                key, draw_next_game_frame = None, False
+                while not draw_next_game_frame:
+                    clock.tick(30) # control FPS
+
+                    for event in pygame.event.get():
+                        if event.type == pygame.KEYDOWN:
+                            key = [False]*1000
+                            key[event.key] = True
+                            if event_handler_func != None:
+                                stop, eyelink_err_code, bool_drawgc = event_handler_func(key)
+                                if stop:
+                                    return eyelink_err_code
+                            a_index = aenum.action_map(key, self.gamename)
+                            # Not in all cases when action_map returns "NO OP" is the real action "NO OP",
+                            # Only when the human press "TAB", is the real action "NO OP".
+                            if (a_index == aenum.PLAYER_A_NOOP and key[pygame.K_TAB]) \
+                            or  a_index != aenum.PLAYER_A_NOOP:
+                                draw_next_game_frame = True
+
+                    # Show game image
+                    cur_frame_np = self.ale.getScreenRGB()
+                    cur_frame_Surface = pygame.surfarray.make_surface(cur_frame_np)
+                    cur_frame_Surface = pygame.transform.flip(cur_frame_Surface, True, False)
+                    cur_frame_Surface = pygame.transform.rotate(cur_frame_Surface, 90)
+                    # Perform scaling directly on screen, leaving cur_frame_Surface unscaled.
+                    # Slightly faster than scaling cur_frame_Surface and then transfer to screen.
+                    pygame.transform.scale(cur_frame_Surface, self.size, self.screen)
+
+                    if gc_window_drawer_func != None and bool_drawgc:
+                        gc_window_drawer_func(self.screen)
+
+                    pygame.display.flip()
+
+                frame_cnt+=1
+                # Save frame to disk (160*210, i.e. not scaled; because this is faster)
+                if save_screen_func != None:
+                    save_screen_func(cur_frame_Surface, frame_cnt)
+
+                # Apply an action and get the resulting reward
+                a_index = aenum.action_map(key, self.gamename)
+                a = self.legal_actions[a_index]
+                reward = self.ale.act(a);
+                total_reward += reward
+                if record_a_and_r_func != None:
+                    record_a_and_r_func(a, reward)
+
+                pygame.event.pump() # need this line to get new key pressed
+
+            print 'Episode', episode, 'ended with score:', total_reward
+            self.ale.reset_game()
 
         TRIAL_OK = 0 # copied from EyeLink's constant
         return TRIAL_OK
--- mypylink/alerecord_main.py
+++ mypylink/alerecord_main.py
@@ -1,74 +1,303 @@
-from pylink import *
-import time
-import gc
-import sys
-import gcwindow_trial
-from pygame import display
+import time, gc, sys
 from EyeLinkCoreGraphicsPyGame import EyeLinkCoreGraphicsPyGame
+from pylink import *
+from pygame import *
+from aleForET import aleForET
 from IPython import embed
+from ScreenRecorder import ScreenRecorder
+import action_enums
 
-try:
-  eyelinktracker = EyeLink("100.1.1.1")
-except Exception as ex:
-	print(str(ex) + "\nEL is None, using dummy EyeLink interface")
-        eyelinktracker = EyeLink(None)
 
-if len(sys.argv) < 2:
-	print 'Usage:', sys.argv[0], 'rom_file'
-	sys.exit()
-rom_file = sys.argv[1]
+RIGHT_EYE = 1
+LEFT_EYE = 0
+BINOCULAR = 2
+step_by_step_mode = None
+scr_recorder = ScreenRecorder()
 
-genv = EyeLinkCoreGraphicsPyGame(960,630,eyelinktracker)
-openGraphicsEx(genv)
+# A class used to store the states required by drawgc()
+class drawgc_wrapper:
+	def __init__(self):
+		self.cursorsize = (15, 15)
+		self.cursor = Surface(self.cursorsize)
+		self.cursor.fill((255, 255, 255, 255))
+		draw.circle(self.cursor, (255, 0, 0), (self.cursorsize[0] // 2, self.cursorsize[1] // 2) , 5)
 
-#Opens the EDF file.
-edfFileName = "ATARI.EDF";
-getEYELINK().openDataFile(edfFileName)		
+		eye_used = getEYELINK().eyeAvailable() #determine which eye(s) are available 
+		# TODO do we only track one eye?
+		if eye_used == RIGHT_EYE:
+			getEYELINK().sendMessage("EYE_USED 1 RIGHT")
+		elif eye_used == LEFT_EYE or eye_used == BINOCULAR:
+			getEYELINK().sendMessage("EYE_USED 0 LEFT")
+			eye_used = LEFT_EYE
+		else:
+			raise Exception("Error in getting the eye information!")
+		self.eye_used = eye_used
+
+	def drawgc(self, surf):
+		'''Does gaze-contingent drawing; uses getNewestSample() to get latest update '''
+		dt = getEYELINK().getNewestSample() # check for new sample update
+		if(dt != None):
+			# Gets the gaze position of the latest sample
+			if self.eye_used == RIGHT_EYE and dt.isRightSample():
+				gaze_position = dt.getRightEye().getGaze()
+			elif self.eye_used == LEFT_EYE and dt.isLeftSample():
+				gaze_position = dt.getLeftEye().getGaze()
+
+			# Determines the top-left corner
+			region_topleft = (gaze_position[0] - self.cursorsize[0] // 2, gaze_position[1] - self.cursorsize[1] // 2)
+
+			surf.blit(self.cursor, region_topleft) # Draws and shows the cursor content;
+
+def do_trail_subfunc_starting_msg(gamename):
+	# This controls the title at the bottom of the eyetracker display
+	getEYELINK().sendCommand("record_status_message 'Trial %s'" % (gamename))	
+	# Always send a TRIALID message before starting to record.
+	# EyeLink Data Viewer defines the start of a trial by the TRIALID message.
+	# This message is different than the start of recording message START that is logged when the trial recording begins. 
+	# The Data viewer will not parse any messages, events, or samples, that exist in the data file prior to this message.
+	getEYELINK().sendMessage("TRIALID %s" % gamename)
+
+def do_trail_subfunc_start_recording():
+	error = getEYELINK().startRecording(1, 1, 1, 1)
+	if error:	return error
+	try: 
+		getEYELINK().waitForBlockStart(100,1,0) 
+	except RuntimeError: 
+		if getLastError()[0] == 0: # wait time expired without link data 
+			getEYELINK().stopRecording()
+			print ("ERROR: No link samples received!") 
+			return TRIAL_ERROR 
+		else: # for any other status simply re-raise the exception 
+			raise
+	return 0
+
+def do_trail_subfunc_starting_msg2():
+	# according to pylink.chm:
+	# "SYNCTIME" marks the zero-time in a trial. A number may follow, which 
+	# is interpreted as the delay of the message from the actual stimulus onset. 
+	# It is suggested that recording start 100 milliseconds before the display is
+	# drawn or unblanked at zero-time, so that no data at the trial start is lost.
+	getEYELINK().sendMessage("SYNCTIME %d" % 0) # From above doc it seems we can just send 0 because we haven't drawn anything yet
+
+def do_trail_subfunc_end_recording():
+	pumpDelay(100) # adds 100 msec of data to catch final events
+	getEYELINK().stopRecording()
+	while getEYELINK().getkey():
+		pass
+	# The TRIAL_RESULT message defines the end of a trial for the EyeLink Data Viewer. 
+	# This is different than the end of recording message END that is logged when the trial recording ends. 
+	# Data viewer will not parse any messages, events, or samples that exist in the data file after this message. 
+	getEYELINK().sendMessage("TRIAL_RESULT %d" % 0)
+
+def do_trial(surf, ale):
+
+	play_beep_func(0, repeat=5)
+	try:
+		getEYELINK().doDriftCorrect(surf.get_rect().w // 2, surf.get_rect().h // 2, 1, 0)
+	except: # When ESC is pressed or "Abort" buttun clicked, an exception will be thrown
+		pass
+
+	do_trail_subfunc_starting_msg(ale.gamename)
+	err = do_trail_subfunc_start_recording()
+	if err != 0: return err
+	do_trail_subfunc_starting_msg2()
+
+	surf.fill((255, 255, 255, 255))
+	getEYELINK().flushKeybuttons(0)
+	gc.disable() # disable python garbage collection for the entire experiment
+
+	# experiment starts
+	dw = drawgc_wrapper()
+	if step_by_step_mode:
+		eyelink_err_code = ale.run_in_step_by_step_mode(dw.drawgc, save_screen_callback_func, event_handler_callback_func, record_a_and_r_callback_func)
+	else:
+		eyelink_err_code = ale.run(dw.drawgc, save_screen_callback_func, event_handler_callback_func, record_a_and_r_callback_func)
+
+	# experiment ends
+	gc.enable()
+	do_trail_subfunc_end_recording()
+	if eyelink_err_code == 0:
+		eyelink_err_code = getEYELINK().getRecordingStatus() # This function has something to say, so get its err_code (see api doc for details)  
+	print "Trial Ended. eyelink_err_code = %d (%s) Compressing recorded frames..." % (eyelink_err_code, eyelink_err_code_to_str(eyelink_err_code))
 	
-flushGetkeyQueue(); 
-getEYELINK().setOfflineMode();                          
+	scr_recorder.compress_record_dir(remove_original_dir=True)
 
-#Gets the display surface and sends a mesage to EDF file;
-surf = display.get_surface()
-getEYELINK().sendCommand("screen_pixel_coords =  0 0 %d %d" %(surf.get_rect().w - 1, surf.get_rect().h - 1))
-getEYELINK().sendMessage("DISPLAY_COORDS  0 0 %d %d" %(surf.get_rect().w - 1, surf.get_rect().h - 1))
+	# After the recording is done, we should run "Validation".
+	# So here doTrackerSetup() is called, and then the experimenter clicks "Validate". If the validation result is 
+	# good, do nothing. If bad, the experimenter marks this experiment as "bad" on paper. (the code won't do the marking, for now)
+	play_beep_func(1, repeat=5)
+	print "Switched the eye tracker to Setup menu. Now you may perform validation."
+	getEYELINK().doTrackerSetup()
+	return eyelink_err_code
 
-tracker_software_ver = 0
-eyelink_ver = getEYELINK().getTrackerVersion()
-if eyelink_ver == 3:
-	tvstr = getEYELINK().getTrackerVersionString()
-	vindex = tvstr.find("EYELINK CL")
-	tracker_software_ver = int(float(tvstr[(vindex + len("EYELINK CL")):].strip()))
+
+def save_screen_callback_func(screen, frameid):
+	# the drawing of current frame should align with the recording of eye
+	# position as close as possible. So we put sendMessage at the first line.
+	getEYELINK().sendMessage("SCR_RECORDER FRAMEID %d" % frameid)
+	global scr_recorder
+	scr_recorder.save(screen, frameid)
+
+bool_drawgc = False
+def event_handler_callback_func(key_pressed):
+	global bool_drawgc
+	# First check if host PC is still recording
+	# This will block the thread when "abort trial" is clicked at host PC and the "abort trail" menu is shown 
+	error = getEYELINK().isRecording() 
+	if error != 0: # happens when "abort trial" is clicked at host PC
+		return True, error, bool_drawgc
+
+	if key_pressed[K_ESCAPE]:
+		print("Exiting the game...")
+		getEYELINK().sendMessage("key_pressed non-atari esc")
+		return True, SKIP_TRIAL, bool_drawgc
+	elif key_pressed[K_F1]:
+		print("Pause the game...") # TODO how to pause the game? cannot run doTrackerSetup(), it will stop recording
+		getEYELINK().sendMessage("key_pressed non-atari pause")
+		getEYELINK().doTrackerSetup()
+	elif key_pressed[K_F5]:
+		print("Calibrate....")
+		getEYELINK().sendMessage("key_pressed non-atari calibrate")
+		getEYELINK().doTrackerSetup()
+	elif key_pressed[K_F7]:
+		print("Showing gaze-contigent window....")
+		getEYELINK().sendMessage("key_pressed non-atari gcwindowON")
+		bool_drawgc = True
+	elif key_pressed[K_F8]:
+		print("Hiding gaze-contigent window....")
+		getEYELINK().sendMessage("key_pressed non-atari gcwindowOFF")
+		bool_drawgc = False
+
+	return False, None, bool_drawgc
+
+def record_a_and_r_callback_func(atari_action, reward):
+	getEYELINK().sendMessage("key_pressed atari_action %d" % (atari_action))
+	getEYELINK().sendMessage("reward %d" % (reward))
+
+def run_trials(rom_file, screen):
+	''' This function is used to run all trials and handles the return value of each trial. '''
+
+	ale = aleForET(rom_file, screen)
+
+	# Show tracker setup screen at the beginning of the experiment.
+	# It won't return unitl we press ESC on display PC or click "Exit Setup" on host PC
+	play_beep_func(2, repeat=2)
+	getEYELINK().doTrackerSetup()
+
+	# Determine whether to redo/finish/quit the trial, depending on the return value
+	# These return values are predefined in PyLink, so they might also be read by Data Viewer (I'm not sure).
+	# Some are returned when (for example) we click "Abort Trial" and then "Abort Experiment" or "Repeat Trial" on host PC
+	while 1:
+		ret_value = do_trial(screen, ale)
+		endRealTimeMode()
 	
+		if (ret_value == TRIAL_OK):
+			getEYELINK().sendMessage("TRIAL OK")
+			break
+		elif (ret_value == SKIP_TRIAL):
+			getEYELINK().sendMessage("TRIAL ABORTED")
+			break			
+		elif (ret_value == ABORT_EXPT):
+			getEYELINK().sendMessage("EXPERIMENT ABORTED")
+			break
+		elif (ret_value == REPEAT_TRIAL):
+			getEYELINK().sendMessage("TRIAL REPEATED")
+		else: 
+			getEYELINK().sendMessage("TRIAL ERROR")
+			break
 
-if eyelink_ver>=2:
-	getEYELINK().sendCommand("select_parser_configuration 0")
-	if eyelink_ver == 2: #turn off scenelink camera stuff
-		getEYELINK().sendCommand("scene_camera_gazemap = NO")
-else:
-	getEYELINK().sendCommand("saccade_velocity_threshold = 35")
-	getEYELINK().sendCommand("saccade_acceleration_threshold = 9500")
-	
-# set EDF file contents 
-getEYELINK().sendCommand("file_event_filter = LEFT,RIGHT,FIXATION,SACCADE,BLINK,MESSAGE,BUTTON,INPUT")
-if tracker_software_ver>=4:
-	getEYELINK().sendCommand("file_sample_data  = LEFT,RIGHT,GAZE,AREA,GAZERES,STATUS,HTARGET,INPUT")
-else:
-	getEYELINK().sendCommand("file_sample_data  = LEFT,RIGHT,GAZE,AREA,GAZERES,STATUS,INPUT")
+	# Experiment ended
+	if getEYELINK() != None:
+		# File transfer and cleanup!
+		getEYELINK().setOfflineMode();                          
+		msecDelay(500);                 
 
-# set link data (used for gaze cursor) 
-getEYELINK().sendCommand("link_event_filter = LEFT,RIGHT,FIXATION,SACCADE,BLINK,BUTTON,INPUT")
-if tracker_software_ver>=4:
-	getEYELINK().sendCommand("link_sample_data  = LEFT,RIGHT,GAZE,GAZERES,AREA,STATUS,HTARGET,INPUT")
-else:
-	getEYELINK().sendCommand("link_sample_data  = LEFT,RIGHT,GAZE,GAZERES,AREA,STATUS,INPUT")
-	
-	
-# Start the experiment!
+		#Close the file and transfer it to Display PC
+		getEYELINK().closeDataFile()
+		getEYELINK().receiveDataFile(edfFileName, scr_recorder.dir+".edf")
+		getEYELINK().close();
+	return 0
+		
+def eyelink_err_code_to_str(code):
+	if (code == TRIAL_OK):
+		return "TRIAL OK"
+	elif (code == SKIP_TRIAL):
+		return "TRIAL ABORTED (SKIP_TRIAL)"
+	elif (code == ABORT_EXPT):
+		return "EXPERIMENT ABORTED (ABORT_EXPT)"
+	elif (code == REPEAT_TRIAL):
+		return "TRIAL REPEATED (REPEAT_TRIAL)"
+	else:
+		return "TRIAL ERROR"
 
-if(getEYELINK().isConnected() and not getEYELINK().breakPressed()):
-	gcwindow_trial.set_play_beep_func(genv.play_beep2)
-	gcwindow_trial.run_trials(rom_file, surf)
+def set_play_beep_func(func): 
+	global play_beep_func # used by below code to play a sound
+	play_beep_func=func
 
-#Close the experiment graphics	
-display.quit()
+if __name__ == "__main__":
+	try:
+	  eyelinktracker = EyeLink("100.1.1.1")
+	except Exception as ex:
+		print(str(ex) + '\nEyeLink("100.1.1.1") is None, using dummy EyeLink interface')
+	        eyelinktracker = EyeLink(None)
+
+	if len(sys.argv) < 3:
+		print 'Usage:', sys.argv[0], 'rom_file', "step_by_step_mode(true|false)"
+		sys.exit()
+	rom_file = sys.argv[1]
+	step_by_step_mode = sys.argv[2].upper() == "TRUE"
+
+	genv = EyeLinkCoreGraphicsPyGame(960,630,eyelinktracker)
+	openGraphicsEx(genv)
+
+	#Opens the EDF file.
+	edfFileName = "ATARI.EDF";
+	getEYELINK().openDataFile(edfFileName)		
+		
+	flushGetkeyQueue(); 
+	getEYELINK().setOfflineMode();                          
+
+	#Gets the display surface and sends a mesage to EDF file;
+	surf = display.get_surface()
+	getEYELINK().sendCommand("screen_pixel_coords =  0 0 %d %d" %(surf.get_rect().w - 1, surf.get_rect().h - 1))
+	getEYELINK().sendMessage("DISPLAY_COORDS  0 0 %d %d" %(surf.get_rect().w - 1, surf.get_rect().h - 1))
+
+	tracker_software_ver = 0
+	eyelink_ver = getEYELINK().getTrackerVersion()
+	if eyelink_ver == 3:
+		tvstr = getEYELINK().getTrackerVersionString()
+		vindex = tvstr.find("EYELINK CL")
+		tracker_software_ver = int(float(tvstr[(vindex + len("EYELINK CL")):].strip()))
+		
+
+	if eyelink_ver>=2:
+		getEYELINK().sendCommand("select_parser_configuration 0")
+		if eyelink_ver == 2: #turn off scenelink camera stuff
+			getEYELINK().sendCommand("scene_camera_gazemap = NO")
+	else:
+		getEYELINK().sendCommand("saccade_velocity_threshold = 35")
+		getEYELINK().sendCommand("saccade_acceleration_threshold = 9500")
+		
+	# set EDF file contents 
+	getEYELINK().sendCommand("file_event_filter = LEFT,RIGHT,FIXATION,SACCADE,BLINK,MESSAGE,BUTTON,INPUT")
+	if tracker_software_ver>=4:
+		getEYELINK().sendCommand("file_sample_data  = LEFT,RIGHT,GAZE,AREA,GAZERES,STATUS,HTARGET,INPUT")
+	else:
+		getEYELINK().sendCommand("file_sample_data  = LEFT,RIGHT,GAZE,AREA,GAZERES,STATUS,INPUT")
+
+	# set link data (used for gaze cursor) 
+	getEYELINK().sendCommand("link_event_filter = LEFT,RIGHT,FIXATION,SACCADE,BLINK,BUTTON,INPUT")
+	if tracker_software_ver>=4:
+		getEYELINK().sendCommand("link_sample_data  = LEFT,RIGHT,GAZE,GAZERES,AREA,STATUS,HTARGET,INPUT")
+	else:
+		getEYELINK().sendCommand("link_sample_data  = LEFT,RIGHT,GAZE,GAZERES,AREA,STATUS,INPUT")
+		
+		
+	# Start the experiment!
+
+	if(getEYELINK().isConnected() and not getEYELINK().breakPressed()):
+		set_play_beep_func(genv.play_beep2)
+		run_trials(rom_file, surf)
+
+	#Close the experiment graphics	
+	display.quit()
