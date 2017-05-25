86bbff4bbf332bf6c0793188fb17686d5c030c32
Ruohan Zhang <zharucs@cs.utexas.edu>
2017-05-25 10:32:48 -0500

    make gocondor.py work on zharucs

--- modeling/gocondor.py
+++ modeling/gocondor.py
@@ -11,6 +11,7 @@ import sys, re, os, subprocess
 basestr="""
 # doc at : http://research.cs.wisc.edu/htcondor/manual/current/condor_submit.html
 arguments = /scratch/cluster/zharucs/ale/modeling/{0}
+remote_initialdir = /scratch/cluster/zharucs/ale/modeling/
 +Group ="GRAD"
 +Project ="AI_ROBOTICS"
 +ProjectDescription="ale"
@@ -20,7 +21,7 @@ Universe = vanilla
 # UTCS has 18 such machine, to take a look, run 'condor_status  -constraint 'GTX1080==true' 
 Requirements=(TARGET.GTX1080== true)
 
-executable = /u/zhuode93/anaconda2/bin/ipython 
+executable = /u/zharucs/anaconda2/bin/ipython 
 getenv = true
 output = CondorOutput/$(Cluster).out
 error = CondorOutput/$(Cluster).err
