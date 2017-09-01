39a5c7d57aadd195271b5125986247b5ddb5f11f
Zhuode Liu <zhuode93@cs.utexas.edu>
2017-09-01 00:56:59 -0500

    update main-base.py to accept +BASE_FILE_NAME +MODEL_DIR +dropout from command line

--- modeling/main-base.py
+++ modeling/main-base.py
@@ -5,22 +5,23 @@ from IPython import embed
 import input_utils, misc_utils as MU
 import ipdb
 
-NUM_CLASSES=8
-BASE_FILE_NAME = "/scratch/cluster/zharucs/dataset_gaze/cat{36_38_39_43_RZ}tr_{37_RZ}val"
+NUM_CLASSES=18
+# BASE_FILE_NAME = "/scratch/cluster/zharucs/dataset_gaze/old_data/cat{36_38_39_43_RZ}tr_{37_RZ}val"
+BASE_FILE_NAME = sys.argv[1]
 LABELS_FILE_TRAIN = BASE_FILE_NAME + '-train.txt'
 LABELS_FILE_VAL =  BASE_FILE_NAME + '-val.txt'
 GAZE_POS_ASC_FILE = BASE_FILE_NAME + '.asc'
 SHAPE = (84,84,1) # height * width * channel This cannot read from file and needs to be provided here
 BATCH_SIZE=100
 num_epoch = 50
-dropout = float(sys.argv[1])
-MODEL_DIR = 'Seaquest_36-43_37'
+dropout = float(sys.argv[3])
+MODEL_DIR = sys.argv[2]
 resume_model = False
 
 MU.save_GPU_mem_keras()
 MU.keras_model_serialization_bug_fix()
 
-expr = MU.ExprCreaterAndResumer(MODEL_DIR,postfix="baseline_dr%s" % sys.argv[1])
+expr = MU.ExprCreaterAndResumer(MODEL_DIR,postfix="run1_FVs_dr%s" % str(dropout))
 expr.redirect_output_to_logfile_if_not_on("eldar-11")
 
 if resume_model:
@@ -29,23 +30,23 @@ if resume_model:
 else:
     inputs=L.Input(shape=SHAPE)
     x=inputs # inputs is used by the line "Model(inputs, ... )" below
-    x=L.Conv2D(20, (8,8), strides=4, padding='same')(x)
+    x=L.Conv2D(32, (8,8), strides=4, padding='same')(x)
     x=L.BatchNormalization()(x)
     x=L.Activation('relu')(x)
     x=L.Dropout(dropout)(x)
     
-    x=L.Conv2D(40, (4,4), strides=2, padding='same')(x)
+    x=L.Conv2D(64, (4,4), strides=2, padding='same')(x)
     x=L.BatchNormalization()(x)
     x=L.Activation('relu')(x)
     x=L.Dropout(dropout)(x)
     
-    x=L.Conv2D(80, (3,3), strides=2, padding='same')(x)
+    x=L.Conv2D(64, (3,3), strides=2, padding='same')(x)
     x=L.BatchNormalization()(x)
     x=L.Activation('relu')(x)
     x=L.Dropout(dropout)(x)
     x=L.Flatten()(x)
     
-    x=L.Dense(256, activation='relu')(x)
+    x=L.Dense(512, activation='relu')(x)
     x=L.Dropout(dropout)(x)
     logits=L.Dense(NUM_CLASSES, name="logits")(x)
     prob=L.Activation('softmax', name="prob")(logits)
@@ -59,15 +60,15 @@ else:
 expr.dump_src_code_and_model_def(sys.argv[0], model)
 
 d=input_utils.Dataset(LABELS_FILE_TRAIN, LABELS_FILE_VAL, SHAPE)
-embed()
+
 model.fit(d.train_imgs, d.train_lbl, BATCH_SIZE, epochs=num_epoch,
-    validation_data=(d.val_imgs, d.val_lbl),
-    shuffle=True,verbose=2,
+    validation_data=(d.val_imgs, d.val_lbl, d.val_weight),
+    shuffle=True, sample_weight=d.train_weight, verbose=2,
     callbacks=[K.callbacks.TensorBoard(log_dir=expr.dir),
         K.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.5,patience=3, min_lr=0.001),
         MU.PrintLrCallback()])
 
 # expr.save_weight_and_training_config_state(model) # uncomment this line if you want to save model
 
-score = model.evaluate(d.val_imgs, d.val_lbl, BATCH_SIZE, 0)
+score = model.evaluate(d.val_imgs, d.val_lbl, BATCH_SIZE, 0, sample_weight=d.val_weight)
 expr.printdebug("eval score:" + str(score))
