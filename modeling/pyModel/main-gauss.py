307c2047387c43baf9d3fcf99e3aed2fa7d9e5b0
zerolocker <zero-lzd@163.com>
2017-09-18 08:31:07 -0700

    Update main-gauss.py

--- modeling/pyModel/main-gauss.py
+++ modeling/pyModel/main-gauss.py
@@ -15,7 +15,6 @@ BATCH_SIZE=100
 num_epoch = 50
 dropout = 0.25
 MODEL_DIR = 'Breakout_42_44'
-resume_model = False
 sigma=25
 background=0.0
 
@@ -26,43 +25,39 @@ expr = MU.ExprCreaterAndResumer(MODEL_DIR,postfix="Gauss_BG%d_gCUR_gauss%d" % (b
 
 expr.redirect_output_to_logfile_if_not_on("eldar-11")
 
-if resume_model:
-    model = expr.load_weight_and_training_config_and_state()
-    expr.printdebug("Checkpoint found. Resuming model at %s" % expr.dir_lasttime)
-else:
-    gaze_heatmaps = L.Input(shape=(SHAPE[0],SHAPE[1],1))
-    g=gaze_heatmaps
+gaze_heatmaps = L.Input(shape=(SHAPE[0],SHAPE[1],1))
+g=gaze_heatmaps
 
-    imgs=L.Input(shape=SHAPE)
-    x=imgs
-    x=L.Multiply()([x,g])
-    x_intermediate=x
-    x=L.Conv2D(20, (8,8), strides=4, padding='same')(x)
-    x=L.BatchNormalization()(x)
-    x=L.Activation('relu')(x)
-    x=L.Dropout(dropout)(x)
+imgs=L.Input(shape=SHAPE)
+x=imgs
+x=L.Multiply()([x,g])
+x_intermediate=x
+x=L.Conv2D(20, (8,8), strides=4, padding='same')(x)
+x=L.BatchNormalization()(x)
+x=L.Activation('relu')(x)
+x=L.Dropout(dropout)(x)
 
-    x=L.Conv2D(40, (4,4), strides=2, padding='same')(x)
-    x=L.BatchNormalization()(x)
-    x=L.Activation('relu')(x)
-    x=L.Dropout(dropout)(x)
+x=L.Conv2D(40, (4,4), strides=2, padding='same')(x)
+x=L.BatchNormalization()(x)
+x=L.Activation('relu')(x)
+x=L.Dropout(dropout)(x)
 
-    x=L.Conv2D(80, (3,3), strides=2, padding='same')(x)
-    x=L.BatchNormalization()(x)
-    x=L.Activation('relu')(x)
-    x=L.Dropout(dropout)(x)
-    x=L.Flatten()(x)
-    
-    x=L.Dense(256, activation='relu')(x)
-    x=L.Dropout(dropout)(x)
-    logits=L.Dense(NUM_CLASSES, name="logits")(x)
-    prob=L.Activation('softmax', name="prob")(logits)
-    model=Model(inputs=[imgs, gaze_heatmaps], outputs=[logits, prob, g, x_intermediate])
+x=L.Conv2D(80, (3,3), strides=2, padding='same')(x)
+x=L.BatchNormalization()(x)
+x=L.Activation('relu')(x)
+x=L.Dropout(dropout)(x)
+x=L.Flatten()(x)
 
-    opt=K.optimizers.Adadelta(lr=1.0, rho=0.95, epsilon=1e-08, decay=0.0)
+x=L.Dense(256, activation='relu')(x)
+x=L.Dropout(dropout)(x)
+logits=L.Dense(NUM_CLASSES, name="logits")(x)
+prob=L.Activation('softmax', name="prob")(logits)
+model=Model(inputs=[imgs, gaze_heatmaps], outputs=[logits, prob, g, x_intermediate])
 
-    model.compile(loss={"prob":None, "logits": MU.loss_func},
-                 optimizer=opt,metrics={"logits": MU.acc_})
+opt=K.optimizers.Adadelta(lr=1.0, rho=0.95, epsilon=1e-08, decay=0.0)
+
+model.compile(loss={"prob":None, "logits": MU.loss_func},
+             optimizer=opt,metrics={"logits": MU.acc_})
 
 expr.dump_src_code_and_model_def(sys.argv[0], model)
 
