f2efa57dc0b7ab4143a7184410b02e8f8886a178
zerolocker <zero-lzd@163.com>
2017-09-18 08:25:37 -0700

    Update main-SmoothLabel.py

--- modeling/pyModel/main-SmoothLabel.py
+++ modeling/pyModel/main-SmoothLabel.py
@@ -15,7 +15,6 @@ BATCH_SIZE=100
 num_epoch = 25 
 dropout = 0.5
 MODEL_DIR = 'Seaquest_36_37'
-resume_model = False
 
 MU.save_GPU_mem_keras()
 MU.keras_model_serialization_bug_fix()
@@ -23,38 +22,34 @@ MU.keras_model_serialization_bug_fix()
 expr = MU.ExprCreaterAndResumer(MODEL_DIR,postfix="baseline_smlabel")
 expr.redirect_output_to_logfile_if_not_on("eldar-11")
 
-if resume_model:
-    model = expr.load_weight_and_training_config_and_state()
-    expr.printdebug("Checkpoint found. Resuming model at %s" % expr.dir_lasttime)
-else:
-    inputs=L.Input(shape=SHAPE)
-    x=inputs # inputs is used by the line "Model(inputs, ... )" below
-    x=L.Conv2D(20, (8,8), strides=4, padding='same')(x)
-    x=L.BatchNormalization()(x)
-    x=L.Activation('relu')(x)
-    x=L.Dropout(dropout)(x)
-    
-    x=L.Conv2D(40, (4,4), strides=2, padding='same')(x)
-    x=L.BatchNormalization()(x)
-    x=L.Activation('relu')(x)
-    x=L.Dropout(dropout)(x)
-    
-    x=L.Conv2D(80, (3,3), strides=2, padding='same')(x)
-    x=L.BatchNormalization()(x)
-    x=L.Activation('relu')(x)
-    x=L.Dropout(dropout)(x)
-    x=L.Flatten()(x)
-    
-    x=L.Dense(256, activation='relu')(x)
-    x=L.Dropout(dropout)(x)
-    logits=L.Dense(NUM_CLASSES, name="logits")(x)
-    prob=L.Activation('softmax', name="prob")(logits)
-    model=Model(inputs=inputs, outputs=[logits, prob])
+inputs=L.Input(shape=SHAPE)
+x=inputs # inputs is used by the line "Model(inputs, ... )" below
+x=L.Conv2D(20, (8,8), strides=4, padding='same')(x)
+x=L.BatchNormalization()(x)
+x=L.Activation('relu')(x)
+x=L.Dropout(dropout)(x)
 
-    opt=K.optimizers.Adadelta(lr=1.0, rho=0.95, epsilon=1e-08, decay=0.0)
+x=L.Conv2D(40, (4,4), strides=2, padding='same')(x)
+x=L.BatchNormalization()(x)
+x=L.Activation('relu')(x)
+x=L.Dropout(dropout)(x)
 
-    model.compile(loss={"prob":None, "logits": MU.loss_func_nonsparse},
-                 optimizer=opt,metrics={"logits": MU.acc_nonsparse_wrong})
+x=L.Conv2D(80, (3,3), strides=2, padding='same')(x)
+x=L.BatchNormalization()(x)
+x=L.Activation('relu')(x)
+x=L.Dropout(dropout)(x)
+x=L.Flatten()(x)
+
+x=L.Dense(256, activation='relu')(x)
+x=L.Dropout(dropout)(x)
+logits=L.Dense(NUM_CLASSES, name="logits")(x)
+prob=L.Activation('softmax', name="prob")(logits)
+model=Model(inputs=inputs, outputs=[logits, prob])
+
+opt=K.optimizers.Adadelta(lr=1.0, rho=0.95, epsilon=1e-08, decay=0.0)
+
+model.compile(loss={"prob":None, "logits": MU.loss_func_nonsparse},
+             optimizer=opt,metrics={"logits": MU.acc_nonsparse_wrong})
 
 expr.dump_src_code_and_model_def(sys.argv[0], model)
 d=input_utils.Dataset(LABELS_FILE_TRAIN, LABELS_FILE_VAL, SHAPE)
