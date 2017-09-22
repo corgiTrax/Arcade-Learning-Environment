6ad34feade9e0afb751a55ae0800784da90ebac5
Zhuode Liu <zhuode93@cs.utexas.edu>
2017-09-22 02:13:21 -0500

    improve gocondor.py main.py main-base.py to give good examples

--- modeling/main.py
+++ modeling/main.py
@@ -5,79 +5,89 @@ from IPython import embed
 import input_utils, misc_utils as MU
 import ipdb
 
-NUM_CLASSES=8
-BASE_FILE_NAME = "/scratch/cluster/zharucs/dataset_gaze/cat{36_38_39_43_RZ}tr_{37_RZ}val"
-LABELS_FILE_TRAIN = BASE_FILE_NAME + '-train.txt' 
-LABELS_FILE_VAL =  BASE_FILE_NAME + '-val.txt' 
+# This file runs a "fv2ch" model that produces good but not the best result on our AAAI paper.
+# It can be run with gocondor.py <main*.py>, a script submiting condor jobs which call this file
+# with different paramenters. This file can also be run independently.
+
+NUM_CLASSES=18
+BASE_FILE_NAME = "/scratch/cluster/zharucs/dataset_gaze/" + sys.argv[1]
+fv_BASE_FILE_NAME = "/scratch/cluster/zharucs/dataset_action/FVs/" + sys.argv[1]
+LABELS_FILE_TRAIN = BASE_FILE_NAME + '-train.txt'
+LABELS_FILE_VAL =  BASE_FILE_NAME + '-val.txt'
+fv_LABELS_FILE_TRAIN = fv_BASE_FILE_NAME + '-train.txt'
+fv_LABELS_FILE_VAL =  fv_BASE_FILE_NAME + '-val.txt'
 GAZE_POS_ASC_FILE = BASE_FILE_NAME + '.asc'
-SHAPE = (84,84,1) # height * width * channel This cannot read from file and needs to be provided here
+SHAPE = (84,84,1) # height * width * channel. Cannot be read from file; Needs to be provided here
 BATCH_SIZE=100
-num_epoch = 70
-dropout = 0.25
-MODEL_DIR = 'Seaquest_36-43_37'
+num_epoch = 50
+MODEL_DIR = sys.argv[2]
+dropout = float(sys.argv[3])
+save_model = True if '--save' in sys.argv else False # you can specify "--save" in argument
 
-MU.save_GPU_mem_keras()
 MU.keras_model_serialization_bug_fix()
+expr = MU.ExprCreaterAndResumer(MODEL_DIR,postfix="fv2ch_dr%s" % (str(dropout)))
 
-expr = MU.ExprCreaterAndResumer(MODEL_DIR,postfix="predgaze_PreMul")
-expr.redirect_output_to_logfile_if_not_on("eldar-11")
+if True: # I just want to indent
+    inputs=L.Input(shape=SHAPE)
+    x=inputs # inputs is used by the line "Model(inputs, ... )" below
+    x=L.Conv2D(32, (8,8), strides=4, padding='same')(x)
+    x=L.BatchNormalization()(x)
+    x=L.Activation('relu')(x)
+    x=L.Dropout(dropout)(x)
+    
+    x=L.Conv2D(64, (4,4), strides=2, padding='same')(x)
+    x=L.BatchNormalization()(x)
+    x=L.Activation('relu')(x)
+    x=L.Dropout(dropout)(x)
+    
+    x=L.Conv2D(64, (3,3), strides=1, padding='same')(x)
+    x=L.BatchNormalization()(x)
+    x=L.Activation('relu')(x)
+    
+    fvinputs=L.Input(shape=SHAPE)
+    fvx=fvinputs
+    fvx=L.Conv2D(32, (8,8), strides=4, padding='same')(fvx)
+    fvx=L.BatchNormalization()(fvx)
+    fvx=L.Activation('relu')(fvx)
+    fvx=L.Dropout(dropout)(fvx)
+    
+    fvx=L.Conv2D(64, (4,4), strides=2, padding='same')(fvx)
+    fvx=L.BatchNormalization()(fvx)
+    fvx=L.Activation('relu')(fvx)
+    fvx=L.Dropout(dropout)(fvx)
 
-# Now build the model
+    fvx=L.Conv2D(64, (3,3), strides=1, padding='same')(fvx)
+    fvx=L.BatchNormalization()(fvx)
+    fvx=L.Activation('relu')(fvx)
 
-gaze_heatmaps = L.Input(shape=(SHAPE[0],SHAPE[1],1))
-g=gaze_heatmaps
+    x=L.Average()([x,fvx])
+    x=L.Dropout(dropout)(x)
+    x=L.Flatten()(x)
+    x=L.Dense(512, activation='relu')(x)
+    x=L.Dropout(dropout)(x)
+    logits=L.Dense(NUM_CLASSES, name="logits")(x)
+    prob=L.Activation('softmax', name="prob")(logits)
 
-imgs=L.Input(shape=SHAPE)
-x=imgs
-x=L.Multiply()([x,g])
-x_intermediate=x
-x=L.Conv2D(20, (8,8), strides=4, padding='same')(x)
-x=L.BatchNormalization()(x)
-x=L.Activation('relu')(x)
-x=L.Dropout(dropout)(x)
-
-x=L.Conv2D(40, (4,4), strides=2, padding='same')(x)
-x=L.BatchNormalization()(x)
-x=L.Activation('relu')(x)
-x=L.Dropout(dropout)(x)
-
-x=L.Conv2D(80, (3,3), strides=2, padding='same')(x)
-x=L.BatchNormalization()(x)
-x=L.Activation('relu')(x)
-x=L.Dropout(dropout)(x)
-x=L.Flatten()(x)
-
-x=L.Dense(256, activation='relu')(x)
-x=L.Dropout(dropout)(x)
-logits=L.Dense(NUM_CLASSES, name="logits")(x)
-prob=L.Activation('softmax', name="prob")(logits)
-model=Model(inputs=[imgs, gaze_heatmaps], outputs=[logits, prob, g, x_intermediate])
-
-opt=K.optimizers.Adadelta(lr=1.0, rho=0.95, epsilon=1e-08, decay=0.0)
-
-model.compile(loss={"prob":None, "logits": MU.loss_func},
-            optimizer=opt,metrics={"logits": MU.acc_})
+    model=Model(inputs=[inputs, fvinputs], outputs=[logits, prob])
+    opt=K.optimizers.Adadelta(lr=1.0, rho=0.95, epsilon=1e-08, decay=0.0)
+    model.compile(loss={"prob":None, "logits": MU.loss_func},
+                 optimizer=opt,metrics={"logits": MU.acc_})
 
 expr.dump_src_code_and_model_def(sys.argv[0], model)
 
 d=input_utils.Dataset(LABELS_FILE_TRAIN, LABELS_FILE_VAL, SHAPE)
-d.train_imgs, d.val_imgs = d.train_imgs[8:], d.val_imgs[8:]
-d.train_fid,  d.val_fid  = d.train_fid[8:], d.val_fid[8:]
-d.train_lbl,  d.val_lbl  = d.train_lbl[8:], d.val_lbl[8:]
-input_utils.load_predicted_gaze_heatmap_into_dataset_train_GHmap_val_GHmap__temp(
-    '/scratch/cluster/zharucs/ale/gaze/Seaquest_36-43_37_pastK/79_pKf_3D_dp0.5_shape84_k8s1/cat{36_38_39_43_RZ}tr_{37_RZ}val-train-result.npz',
-    '/scratch/cluster/zharucs/ale/gaze/Seaquest_36-43_37_pastK/79_pKf_3D_dp0.5_shape84_k8s1/cat{36_38_39_43_RZ}tr_{37_RZ}val-val-result.npz',
-    dataset_obj=d)
+dfv=input_utils.Dataset(fv_LABELS_FILE_TRAIN, fv_LABELS_FILE_VAL, SHAPE)
 
-model.fit([d.train_imgs, d.train_GHmap], d.train_lbl, BATCH_SIZE, epochs=num_epoch,
-    validation_data=([d.val_imgs, d.val_GHmap], d.val_lbl),
-    shuffle=True,verbose=2,
+model.fit([d.train_imgs,dfv.train_imgs], d.train_lbl, BATCH_SIZE, epochs=num_epoch,
+    validation_data=([d.val_imgs,dfv.val_imgs], d.val_lbl, d.val_weight),
+    shuffle=True, sample_weight=d.train_weight, verbose=2,
     callbacks=[K.callbacks.TensorBoard(log_dir=expr.dir),
         K.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.5,patience=3, min_lr=0.001),
         MU.PrintLrCallback()])
 
-expr.save_weight_and_training_config_state(model) # uncomment this line if you want to save model
 
-score = model.evaluate([d.val_imgs, d.val_GHmap], d.val_lbl, BATCH_SIZE, 0)
+score = model.evaluate([d.val_imgs, dfv.val_imgs], d.val_lbl, BATCH_SIZE, 0, sample_weight=d.val_weight)
 expr.printdebug("eval score:" + str(score))
 
+if save_model:
+  expr.save_weight_and_training_config_state(model)
